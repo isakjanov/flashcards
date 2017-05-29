7eb883caa295e7d01fc4bd00471ba2d6feb763e8
Tim Isakjanov <tim.isakjanov@gmail.com>
2017-05-29 08:53:15 +0600

    Created DataSource interface and FakeCardsDataSource

---
+++ app/src/main/java/projects/sandbox/flashcards/data/source/CardsDataSource.java
@@ -0,0 +1,38 @@
+package projects.sandbox.flashcards.data.source;
+
+import java.util.List;
+
+import projects.sandbox.flashcards.data.FlashCard;
+
+/**
+ * Interface to access flashcards data
+ */
+public interface CardsDataSource {
+
+    interface GetFlashCardsCallback {
+
+        void onCardsLoaded(List<FlashCard> flashCards);
+
+        void onDataNotAvailable();
+    }
+
+    interface GetFlashCardCallback {
+
+        void onCardLoaded(FlashCard flashCard);
+
+        void onDataNotAvailable();
+    }
+
+    interface DeleteFlashCardCallback {
+
+        void onCardDeleted(FlashCard flashCard);
+
+        void onFailedToDeleteCard();
+    }
+
+    void getFlashCards(GetFlashCardsCallback callback);
+
+    void getFlashCard(GetFlashCardCallback callback);
+
+    void deleteFlashCard(DeleteFlashCardCallback callback);
+}
---
+++ app/src/main/java/projects/sandbox/flashcards/data/source/FakeCardsDataSource.java
@@ -0,0 +1,57 @@
+package projects.sandbox.flashcards.data.source;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import projects.sandbox.flashcards.data.FlashCard;
+
+/**
+ * Fake implementation of CardsDataSource. It is used to develop view with mock data
+ */
+public class FakeCardsDataSource implements CardsDataSource {
+
+    private static FakeCardsDataSource mInstance;
+
+    private List<FlashCard> mFlashCards;
+
+    private FakeCardsDataSource() {
+        mFlashCards = new ArrayList<>(Arrays.asList(new FlashCard[] {
+                new FlashCard(1, "Single responsibility principle",
+                        "Class should be responsible for only one functionality."),
+                new FlashCard(2, "Open / close principle",
+                        "entity should be open for extension but close for modifications."),
+                new FlashCard(3, "Liskov substitutioin principle",
+                        "object of superclass type should be replaceable by any object " +
+                                "of subclass types."),
+                new FlashCard(4, "Interface segregation principle",
+                        "it's better to have single purpose interface rather than general purpose."),
+                new FlashCard(5, "Dependency inversion principle", "entities must depend on " +
+                        "abstractions not on concretions. It states that the high level module must" +
+                        " not depend on the low level module, but they should " +
+                        "depend on abstractions.")
+        }));
+    }
+
+    public static FakeCardsDataSource getInstance() {
+        if (mInstance == null) {
+            mInstance = new FakeCardsDataSource();
+        }
+        return mInstance;
+    }
+
+    @Override
+    public void getFlashCards(GetFlashCardsCallback callback) {
+
+    }
+
+    @Override
+    public void getFlashCard(GetFlashCardCallback callback) {
+
+    }
+
+    @Override
+    public void deleteFlashCard(DeleteFlashCardCallback callback) {
+
+    }
+}
