ac64d574bf3ccdde362cfa2650bf98861e937799
Tim Isakjanov <tim.isakjanov@gmail.com>
2017-06-20 17:12:24 +0600

    implemented callbacks in CardsLoader

--- app/src/main/java/projects/sandbox/flashcards/data/source/CardsLoader.java
+++ app/src/main/java/projects/sandbox/flashcards/data/source/CardsLoader.java
@@ -15,18 +15,58 @@ public class CardsLoader extends AsyncTaskLoader<List<FlashCard>> {
 
     private CardsDataSource mDataSource;
 
+    private List<FlashCard> mCards;
+
     public CardsLoader(Context context, CardsDataSource dataSource) {
         super(context);
         mDataSource = dataSource;
     }
 
+    /**
+     * Load Cards in background thread
+     */
     @Override
     public List<FlashCard> loadInBackground() {
-        return null;
+        List<FlashCard> cards = mDataSource.getFlashCards();
+        return cards;
     }
 
     @Override
+    public void deliverResult(List<FlashCard> data) {
+        if (isReset()) {
+            if (data != null) {
+                // release data
+            }
+        }
+
+        mCards = data;
+
+        if (isStarted()) {
+            super.deliverResult(data);
+        }
+    }
+
+    /**
+     * Handles request to start the Loader
+     * */
+    @Override
     protected void onStartLoading() {
-        deliverResult(mDataSource.getFlashCards());
+        if (mCards != null) {
+            deliverResult(mCards);
+        }
+
+        if (mCards == null) {
+            forceLoad();
+        }
+    }
+
+    @Override
+    protected void onStopLoading() {
+        cancelLoad();
+    }
+
+    @Override
+    protected void onReset() {
+        onStopLoading();
     }
 }
